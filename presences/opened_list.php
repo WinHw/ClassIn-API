890de1abf6c06758c0bb0ceb5470a62d89b06abd
Hendry Winata <hendry24680@gmail.com>
2022-12-27 22:06:38 +0700

    add update presence status on opened list endpoint

--- presences/opened_list.php
+++ presences/opened_list.php
@@ -5,53 +5,60 @@
   $level = $_GET['level'];
   $result = [];
   $query_builder = "";
-  
-  switch($level) {
-    case 'student':
-      $query_builder = "
-      SELECT DISTINCT
-      cpr.id as course_id,
-      cd.name as course_name,
-      c.class as course_class,
-      a.name as course_lecturer,
-      cpr.id as presence_id,
-      cpr.start as presence_start,
-      cpr.end as presence_end,
-      cpr.description as presence_description
-      FROM course_presences cpr
-      INNER JOIN courses c ON c.id = cpr.course_id
-      INNER JOIN course_details cd ON cd.code = c.code
-      INNER JOIN accounts a ON a.id = c.lecturer_id
-      INNER JOIN participants_presences pp ON pp.course_presence_id = cpr.id
-      INNER JOIN course_participants cp ON cp.id = pp.participant_id
-      WHERE c.status = 'active' AND cpr.status = 'opened' AND cp.student_id = '$accID'";
-      break;
-    case 'lecturer':
-      $query_builder = "
-      SELECT DISTINCT
-      cpr.id as course_id,
-      cd.name as course_name,
-      c.class as course_class,
-      a.name as course_lecturer,
-      cpr.id as presence_id,
-      cpr.start as presence_start,
-      cpr.end as presence_end,
-      cpr.description as presence_description
-      FROM course_presences cpr
-      INNER JOIN courses c ON c.id = cpr.course_id
-      INNER JOIN course_details cd ON cd.code = c.code
-      INNER JOIN accounts a ON a.id = c.lecturer_id
-      WHERE c.status = 'active' AND cpr.status = 'opened' AND c.lecturer_id = '$accID'";
-      break;
-    default:
-      break;
-  };
 
-  $query_builder .= " ORDER BY cpr.start ASC";
-  $query = mysqli_query($CON, $query_builder);
-  while ($row = mysqli_fetch_assoc($query)) $result[] = $row;
+  $update = mysqli_query($CON, "UPDATE course_presences set status = 'closed' WHERE end < sysdate()");
+  if ($update) {
+    switch($level) {
+      case 'student':
+        $query_builder .= "
+        SELECT DISTINCT
+        cpr.id as course_id,
+        cd.name as course_name,
+        c.class as course_class,
+        a.name as course_lecturer,
+        cpr.id as presence_id,
+        cpr.start as presence_start,
+        cpr.end as presence_end,
+        cpr.description as presence_description
+        FROM course_presences cpr
+        INNER JOIN courses c ON c.id = cpr.course_id
+        INNER JOIN course_details cd ON cd.code = c.code
+        INNER JOIN accounts a ON a.id = c.lecturer_id
+        INNER JOIN participants_presences pp ON pp.course_presence_id = cpr.id
+        INNER JOIN course_participants cp ON cp.id = pp.participant_id
+        WHERE c.status = 'active' AND cpr.status = 'opened' AND cp.student_id = '$accID'";
+        break;
+      case 'lecturer':
+        $query_builder .= "
+        SELECT DISTINCT
+        cpr.id as course_id,
+        cd.name as course_name,
+        c.class as course_class,
+        a.name as course_lecturer,
+        cpr.id as presence_id,
+        cpr.start as presence_start,
+        cpr.end as presence_end,
+        cpr.description as presence_description
+        FROM course_presences cpr
+        INNER JOIN courses c ON c.id = cpr.course_id
+        INNER JOIN course_details cd ON cd.code = c.code
+        INNER JOIN accounts a ON a.id = c.lecturer_id
+        WHERE c.status = 'active' AND cpr.status = 'opened' AND c.lecturer_id = '$accID'";
+        break;
+      default:
+        break;
+    };
   
-  echo json_encode($result);
+    $query_builder .= " ORDER BY cpr.start ASC";
+    $query = mysqli_query($CON, $query_builder);
+    while ($row = mysqli_fetch_assoc($query)) $result[] = $row;
+    
+    echo json_encode($result);
+  } else {
+    http_response_code(500);
+    echo mysqli_error($CON);
+  }
+  
 
   require_once('../close_connection.php');
 ?>
